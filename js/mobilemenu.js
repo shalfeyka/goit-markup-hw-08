b103759df4aa6e52a5af20d77f01d8136e879728
Marina Checheneva <m.checheneva140201@gmail.com>
2022-01-23 16:48:09 +0200

    .

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -1,4 +1,4 @@
-(() => {
+/*(() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
 
@@ -27,4 +27,38 @@
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
   }
-})();
+})(); */
+
+
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // закрытие мобильного меню на более широких экранах в случае изменения ориентации  устройства
+
+  // window.matchMedia('(min-width: 600px)').addEventListener('change', e => {
+  
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
+
